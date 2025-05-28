6ec00441f78fbbfcaee25840f26ac3377cce39d1
SNKJTS <136660347+SNKJTS@users.noreply.github.com>
2025-05-27 23:40:34 -0600

    Implementación de guardado en archivo CSV

--- RA.py
+++ RA.py
@@ -12,9 +12,15 @@ def serverSocketCreation():
 
 def clientConnection(clientSocket, address):
     try:
-        print(f"[NEW CONNECTION] From {address}")
-        data = clientSocket.recv(1024).decode()
-        print(f"[RECEIVED] {data}")
+        data = clientSocket.recv(1024)
+        public_key = serialization.load_pem_public_key(data)
+        pem_key = public_key.public_bytes(encoding=serialization.Encoding.PEM,format=serialization.PublicFormat.SubjectPublicKeyInfo).decode('utf-8')
+        print(f"[RECEIVED] {public_key}")
+        
+        client_id = register_public_key(pem_key)
+        response = f"{client_id}"
+        clientSocket.sendall(response.encode())
+        clientSocket.close()
 
         # !!!
         if "Smart Device: " in data:
